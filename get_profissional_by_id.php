b17ad4ffdee2708313f7b1773070ac656bf45acd
mateusavila <mateus@mateusavila.com.br>
2014-02-02 13:57:24 -0200

    primeiros coodigos

--- get_profissional_by_id.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$nomes['nome'] = array();
-$nomes['nome'][1] = "Mateus Avila Isidoro";
-$nomes['nome'][2] = "Carolina Fontoura Cartana";
-$nomes['nome'][5] = "José da Silva";
-
-echo json_encode(array("nome" => $nomes['nome'][$_GET['id']]));
-?>
